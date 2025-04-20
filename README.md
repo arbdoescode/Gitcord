3c4d1ab42383a4b09aa2494f780bc769f1d1c11a
Arb Koçi <arbbisha@gmail.com>
2025-04-20 18:12:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gitcord
+ Discord bot for GitHub, to help teams of users in a github project to be reminded threw discord tags
---
+++ webhookfirst.py
@@ -0,0 +1,21 @@
+import asyncio 
+import discord 
+from discord import Webhook, AllowedMentions
+import aiohttp 
+import os 
+async def anything(url):
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(url, session=session)
+        embed = discord.Embed(description="This is for employee of month <@881907187247636582> ")
+        await webhook.send(
+            embed=embed,
+            username="Road Maintainer",
+            allowed_mentions=AllowedMentions(users=True)
+        )
+        
+if __name__ == "__main__":
+    url = os.getenv("BOT_WEBHOOK")
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(anything(url))
+    loop.close()
